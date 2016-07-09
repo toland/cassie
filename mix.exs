0b398873665a3789ff97d565cc9654f1bd0f09f2
Phil Toland <phil.toland@gmail.com>
2016-07-08 20:18:24 -0500

    Go to a pure mix based build

--- mix.exs
+++ mix.exs
@@ -5,13 +5,15 @@ defmodule Schemata.Mixfile do
     [app: :schemata,
      version: "0.1.0",
      elixir: "~> 1.3",
+     compilers: [:elixir, :erlang, :app],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
   def application do
-    [applications: [:logger, :cqerl],
+    [description: 'Schemata library for interacting with Cassandra',
+     applications: [:cqerl],
      env: [
        cassandra_hosts: [{"localhost", 9042}],
        cassandra_opts: []
