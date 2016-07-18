6fe51d6d89a2160674917d202645f22d22143e52
Phil Toland <phil.toland@gmail.com>
2016-07-18 16:29:19 -0500

    Fix the implementation in schemata.erl

--- src/schemata.erl
+++ src/schemata.erl
@@ -4,4 +4,8 @@
 
 
 select(Keyspace, Table, Columns, Conditions) ->
-    'Elixir.Schemata':select(Keyspace, Table, Columns, Conditions).
+    'Elixir.Schemata':select(Columns, [
+        {from, Table},
+        {in, Keyspace},
+        {where, Conditions}
+    ]).
