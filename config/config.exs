681f8702ee83c3153abdcb1a043bddfa06ae77da
Phil Toland <phil.toland@gmail.com>
2016-07-18 16:29:43 -0500

    Fix the cqerl config in dev and test

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,21 @@
 use Mix.Config
 
 config :cqerl,
+  text_uuids: true,
   client_groups: [
     client_group: [
-      name: :no_ks,
-      hosts: ['localhost'],
-      opts: [auth: {:cqerl_auth_plain_handler, [{"cassandra", "cassandra"}]}],
+      hosts: ['127.0.0.1'],
+      opts: [
+        auth: {:cqerl_auth_plain_handler, [{'cassandra', 'cassandra'}]}
+      ],
+      clients_per_server: 1
+    ],
+    client_group: [
+      hosts: ['127.0.0.1'],
+      opts: [
+        keyspace: :schemata_test,
+        auth: {:cqerl_auth_plain_handler, [{'cassandra', 'cassandra'}]}
+      ],
       clients_per_server: 1
     ]
   ]
