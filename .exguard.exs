bd64211010c6281c8bedb5b15df1bd4289695923
Phil Toland <phil.toland@gmail.com>
2016-07-18 23:05:43 -0500

    Tweak the ExGuard config

--- .exguard.exs
+++ .exguard.exs
@@ -1,11 +1,14 @@
 use ExGuard.Config
 
 guard("compile", run_on_start: true)
-|> command("mix")
+|> command("mix compile")
 |> watch(~r{\.(erl|ex|exs|eex|xrl|yrl)\z}i)
+|> ignore(~r/_spec\.exs$/)
+|> ignore(~r/_test\.exs$/)
 |> notification(:off)
 
 guard("specs", run_on_start: true)
-|> command("mix spec --cover")
+|> command("mix espec --cover")
 |> watch({~r{lib/(?<dir>.+)/(?<file>.+).ex$}, fn(m) -> "spec/#{m["dir"]}/#{m["file"]}_spec.exs" end})
 |> watch(~r{\.(erl|ex|exs|eex|xrl|yrl)\z}i)
+|> ignore(~r|spec/db/migrations/|)
