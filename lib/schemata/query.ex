9b4f91db5f3510d513abb11f425af880513cf620
Phil Toland <phil.toland@gmail.com>
2016-07-22 20:56:59 -0500

    Added fields to %Query so it matches #cql_query

--- lib/schemata/query.ex
+++ lib/schemata/query.ex
@@ -7,44 +7,57 @@ defmodule Schemata.Query do
   alias Schemata.Result
   alias Schemata.CassandraError
 
-  @type keyspace          :: nil | binary
-  @type table             :: atom | binary
-  @type column            :: atom | binary
-  @type explicit_columns  :: [column]
-  @type columns           :: :all | explicit_columns
-  @type datatype          :: CQErl.datatype
-  @type conditions        :: map
-  @type consistency_level :: CQErl.consistency_level
-  @type statement         :: CQErl.query_statement
-  @type parameter_val     :: CQErl.parameter_val
-  @type value             :: CQErl.parameter_val
-  @type values            :: map
-  @type limit             :: nil | pos_integer
-  @type error             :: {:error, term}
-  @type query_result      :: CQErl.query_result
+  @type keyspace           :: nil | binary
+  @type table              :: atom | binary
+  @type column             :: atom | binary
+  @type explicit_columns   :: [column]
+  @type columns            :: :all | explicit_columns
+  @type datatype           :: CQErl.datatype
+  @type conditions         :: map
+  @type consistency_level  :: CQErl.consistency_level
+  @type serial_consistency :: CQErl.serial_consistency
+  @type statement          :: CQErl.query_statement
+  @type parameter_val      :: CQErl.parameter_val
+  @type value              :: CQErl.parameter_val
+  @type values             :: map
+  @type limit              :: nil | pos_integer
+  @type error              :: {:error, term}
+  @type query_result       :: CQErl.query_result
 
-  @type column_def        :: [{atom, datatype
-                                   | {:set | :list, datatype}
-                                   | {:map, datatype, datatype}}]
-  @type primary_key       :: atom | [[atom] | atom]
-  @type ordering          :: atom | [{atom, :asc | :desc}]
-  @type ks_strategy       :: :simple | :network_topology
-  @type ks_factor         :: {atom | binary, non_neg_integer}
-                           | non_neg_integer
+  @type column_def         :: [{atom, datatype
+                                    | {:set | :list, datatype}
+                                    | {:map, datatype, datatype}}]
+  @type primary_key        :: atom | [[atom] | atom]
+  @type ordering           :: atom | [{atom, :asc | :desc}]
+  @type ks_strategy        :: :simple | :network_topology
+  @type ks_factor          :: {atom | binary, non_neg_integer}
+                            | non_neg_integer
 
   @type t :: %Query{
-    statement:   binary,
-    values:      values,
-    keyspace:    keyspace,
-    consistency: consistency_level
+    statement:            binary,
+    values:               values,
+    keyspace:             keyspace,
+    consistency:          consistency_level,
+    serial_consistency:   serial_consistency,
+    reusable:             boolean,
+    named:                boolean,
+    page_size:            integer,
+    page_state:           term,
+    value_encode_handler: term
   }
 
   @enforce_keys [:statement]
   defstruct [
-    statement:   nil,
-    values:      %{},
-    keyspace:    nil,
-    consistency: :quorum
+    statement:            nil,
+    values:               %{},
+    keyspace:             nil,
+    consistency:          :quorum,
+    serial_consistency:   nil,
+    reusable:             true,
+    named:                false,
+    page_size:            100,
+    page_state:           nil,
+    value_encode_handler: nil
   ]
 
   @callback from_opts(Keyword.t) :: Queryable.t
@@ -91,28 +104,47 @@ defmodule Schemata.Query do
   @doc ""
   @spec to_query(Queryable.t) :: Query.t
   def to_query(struct) do
-    %Query{
+    query = %Query{
       statement:   Queryable.statement(struct),
       values:      Queryable.values(struct),
-      keyspace:    Queryable.keyspace(struct),
-      consistency: Queryable.consistency(struct)
+      keyspace:    Queryable.keyspace(struct)
     }
+
+    c = Queryable.consistency(struct)
+    if c, do: %Query{query | consistency: c}, else: query
   end
 
-  defp to_cql_query(struct) do
-    query =
-      struct
-      |> Map.from_struct
-      |> Enum.reject(fn {_, v} -> is_nil(v) end)
-
+  defp to_cql_query(query) do
     cql_query(
-      statement:   Keyword.get(query, :statement),
-      values:      Keyword.get(query, :values, %{}),
-      keyspace:    Keyword.get(query, :keyspace, :undefined),
-      consistency: Keyword.get(query, :consistency, :quorum),
-      reusable:    true
+      statement:            query.statement,
+      values:               nullify(query.values, :undefined),
+      keyspace:             nullify(query.keyspace, :undefined),
+      consistency:          nullify(query.consistency, :undefined),
+      serial_consistency:   nullify(query.serial_consistency, :undefined),
+      reusable:             nullify(query.reusable, :undefined),
+      named:                nullify(query.named, :undefined),
+      page_size:            nullify(query.page_size, :undefined),
+      page_state:           nullify(query.page_state, :undefined),
+      value_encode_handler: nullify(query.value_encode_handler, :undefined)
     )
   end
+
+  defp nullify(rec), do: nullify(rec, :null)
+  defp nullify(rec, _fallback) when is_map(rec) do
+    rec
+    |> Enum.map(fn {k, v} -> {k, nullify(v)} end)
+    |> Enum.into(%{})
+  end
+  defp nullify(list = [{_key, _value} | _rest], _fallback) do
+    list
+    |> Enum.map(fn {k, v} -> {k, nullify(v)} end)
+  end
+  defp nullify(list = [_value | _rest], fallback) do
+    list
+    |> Enum.map(&nullify(&1, fallback))
+  end
+  defp nullify(nil, fallback), do: fallback
+  defp nullify(other, _fallback), do: other
 
   defimpl Queryable do
     def statement(struct),   do: struct.statement
