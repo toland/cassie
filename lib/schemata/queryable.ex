811beb678259735c1c3d4a8488fbc3802c248920
Phil Toland <phil.toland@gmail.com>
2016-07-22 20:48:07 -0500

    Add a Queryable implementation for strings

--- lib/schemata/queryable.ex
+++ lib/schemata/queryable.ex
@@ -11,3 +11,10 @@ defprotocol Schemata.Queryable do
   @doc "Extracts the consistency level from the query struct"
   def consistency(struct)
 end
+
+defimpl Schemata.Queryable, for: BitString do
+  def statement(string), do: string
+  def values(_string), do: %{}
+  def keyspace(_string), do: nil
+  def consistency(_string), do: nil
+end
