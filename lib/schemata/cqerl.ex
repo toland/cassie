c81d22101073f995d1fc8b160012c307352dc035
Phil Toland <phil.toland@gmail.com>
2016-08-02 17:20:18 -0500

    Add batch and multi query support

--- lib/schemata/cqerl.ex
+++ lib/schemata/cqerl.ex
@@ -1,19 +1,22 @@
 defmodule Schemata.CQErl do
   @moduledoc false
 
-  defmacro __using__(_opts) do
-    quote do
-      import Schemata.CQErl
-      alias :cqerl, as: CQErl
-    end
-  end
-
   require Record
   import Record, only: [defrecord: 2, extract: 2]
 
   defrecord :cql_query,
     extract(:cql_query, from_lib: "cqerl/include/cqerl.hrl")
 
+  defrecord :cql_query_batch,
+    extract(:cql_query_batch, from_lib: "cqerl/include/cqerl.hrl")
+
   defrecord :cql_result,
     extract(:cql_result, from_lib: "cqerl/include/cqerl.hrl")
+
+  defmacro __using__(_opts) do
+    quote do
+      import Schemata.CQErl
+      alias :cqerl, as: CQErl
+    end
+  end
 end
--- lib/schemata/query.ex
+++ lib/schemata/query.ex
@@ -6,8 +6,9 @@ defmodule Schemata.Query do
   alias Schemata.Queryable
   alias Schemata.Result
   alias Schemata.CassandraError
+  require Logger
 
-  @type keyspace           :: nil | binary
+  @type keyspace           :: atom | binary
   @type table              :: atom | binary
   @type column             :: atom | binary
   @type explicit_columns   :: [column]
@@ -109,6 +110,37 @@ defmodule Schemata.Query do
     %{q | serial_consistency: serial_consistency}
   end
 
+  @doc """
+  Execute a query.
+
+  `keyspace` is the keyspace to execute the query in.
+
+  `statement` is a query string where '?' characters are substituted with
+  parameters from the `Values' list.
+
+  `values` is a property list of column name, value pairs. The pairs must be
+  in the same order that the columns are listed in `Query'.
+
+  On successful completion, the function returns `{:ok, :void}` when there are
+  no results to return and `{:ok, result}` when there is. `result` is an
+  abstract datatype that can be passed to functions in the Result module.
+  """
+  @spec run(keyspace, statement, values, consistency_level) :: query_result
+  def run(keyspace, statement, values, consistency) do
+    run(
+      %Query{
+        statement: statement,
+        values: values,
+        keyspace: keyspace,
+        consistency: consistency
+      })
+  end
+
+  @doc ""
+  @spec run(Query.t | Queryable.t) :: query_result
+  def run(%Query{} = query), do: query |> to_cql_query |> CQErl.run_query
+  def run(queryable), do: queryable |> to_query |> run
+
   @doc ""
   @spec run!(Query.t | Queryable.t) :: :void | Result.t
   def run!(query) do
@@ -134,10 +166,88 @@ defmodule Schemata.Query do
     end
   end
 
-  @doc ""
-  @spec run(Query.t | Queryable.t) :: query_result
-  def run(%Query{} = query), do: query |> to_cql_query |> CQErl.run_query
-  def run(queryable), do: queryable |> to_query |> run
+  @doc """
+  Executes a batch query.
+
+  In a batch query, multiple queries are executed atomically, but not in
+  isolation. Note that it is a bad idea to update multiple rows on the same
+  table with a batch for performance reasons. This will likely result in worse
+  performance. Executing the same query multiple times with different data sets
+  performs best when normal prepared queries are used (see
+  {@link multi_query/4})
+
+  This feature is meant for updating multiple tables containing the same
+  denormalized data atomically.
+
+  `keyspace` is the keyspace to execute the query in.
+
+  `queries` is a list of statement/values pairs. The first element is a query
+  string where '?' characters are substituted with parameters from the entries
+  in the values list. The values list is a property list of column name,
+  value pairs. The pairs must be in the same order that the columns are
+  listed in the query.
+
+  On successful completion, the function returns `{:ok, :void}`.
+  """
+  @spec run_batch(keyspace, [{statement, values}], consistency_level) ::
+    {:ok, :void} | {:error, error}
+  # Cassandra throws an exception if you try to batch zero queries.
+  def run_batch(_keyspace, [], _consistency), do: {:ok, :void}
+  def run_batch(keyspace, query_list, consistency) do
+    query_list
+    |> to_cql_query_batch(keyspace, consistency)
+    |> CQErl.run_query
+  end
+
+  @doc """
+  Executes a query statement multiple times with different datasets.
+
+  `keyspace` is the keyspace to execute the query in.
+
+  `statement` is a query statement where '?' characters are substituted with
+  parameters from the entries in the `values` list.
+
+  `values` is a list where each element is a property list of column name,
+  value pairs. The pairs must be in the same order that the columns are
+  listed in `statement'.
+
+  Returns `:ok`.
+  """
+  @spec run_multi(keyspace, statement, [values], consistency_level) :: :ok
+  def run_multi(keyspace, statement, values_list, consistency) do
+    values_list
+    |> Enum.each(&run(keyspace, statement, &1, consistency))
+
+    :ok
+  end
+
+  @doc """
+  Executes multiple queries statements with different datasets.
+
+  This is functionally similar to {@link batch_query/4}, however this
+  function executes the queries individually rather than using C*'s batching
+  system. This is more appropriate (and performant) when operating over
+  multiple bits of data on a single table requiring varied query strings. An
+  example may be where certain rows of data require a TTL setting but others do
+  not. Don't be afraid to use this form if the query strings may be all
+  identical - in this case it will perform exactly the same as {@link
+  multi_query/4} (with the relatively small exception of the extra cost of
+  passing multiple copies of the same query string).
+
+  `keyspace` is the keyspace to execute the query in.
+
+  `query_vals` is a list of tuples `{statement, values}` where `statement` and
+  `values` are as for {@link query/4}.
+
+  Returns `:ok`.
+  """
+  @spec run_multi(keyspace, [{statement, values}], consistency_level) :: :ok
+  def run_multi(keyspace, query_vals, consistency) do
+    query_vals
+    |> Enum.each(fn {s, v} -> run(keyspace, s, v, consistency) end)
+
+    :ok
+  end
 
   @doc ""
   @spec to_query(Queryable.t) :: Query.t
@@ -153,6 +263,7 @@ defmodule Schemata.Query do
   end
 
   defp to_cql_query(query) do
+    log_query(query.statement, query.values)
     cql_query(
       statement:            query.statement,
       values:               nullify(query.values, :undefined),
@@ -165,6 +276,32 @@ defmodule Schemata.Query do
       page_state:           nullify(query.page_state, :undefined),
       value_encode_handler: nullify(query.value_encode_handler, :undefined)
     )
+  end
+
+  defp to_cql_query_batch(query_list, keyspace, consistency) do
+    cql_query_batch(
+      queries:     batch_query_list(keyspace, query_list),
+      consistency: consistency,
+      mode:        :logged
+    )
+  end
+
+  defp batch_query_list(query_list, keyspace) do
+    Enum.map(query_list, fn ({statement, values}) ->
+      log_query(statement, values)
+      cql_query(
+        statement: statement,
+        values:    nullify(values, :undefined),
+        keyspace:  nullify(keyspace, :undefined)
+      )
+    end)
+  end
+
+  defp log_query(statement, values) do
+    :ok = Logger.debug("""
+    Creating CQL query with statement '#{statement}' \
+    and values #{inspect(values)}\
+    """)
   end
 
   defp nullify(rec), do: nullify(rec, :null)
